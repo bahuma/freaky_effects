a93302bba6b0a5d49050021e69865a34040c45d0
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-04-11 15:33:07 +0200

    Removed background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-// Called when the user clicks on the browser action.
-/*	chrome.browserAction.onClicked.addListener(function(tab) {
-		chrome.tabs.insertCSS(null, {
-			file: "lsdmode.css"
-		  });
-	});*/
